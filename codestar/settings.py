fa9c5d3583d0e4957a9b5cc5d68ec41bdecf0289
Kate Mcguane <katemcguane@hotmail.com>
2024-06-21 19:47:06 +0000

    Append allowed hosts to include heroku

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-a4j@x&5_2w_@+u&k2(zm8#d+)4_kup4j5!5qnt@u3@m%ij76c_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-katemcguane-djangosampl-59jqjg4pagb.ws.codeinstitute-ide.net', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-katemcguane-djangosampl-59jqjg4pagb.ws.codeinstitute-ide.net', 
+'.herokuapp.com']
 
 
 # Application definition
